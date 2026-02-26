3e4ec24269eeeaba7eda74b5665f93f594fcafba
Darkone83 <pfoley83@gmail.com>
2026-02-26 09:17:45 -0700

    Cleanup

--- src/TR Demo/TunelScene.h
+++
@@ -1,8 +0,0 @@
-// TunelScene.h - Checkerboard tunnel scene
-
-#pragma once
-
-void TunelScene_Init();
-void TunelScene_Shutdown();
-void TunelScene_Render(float demoTime);
-bool TunelScene_IsFinished();
